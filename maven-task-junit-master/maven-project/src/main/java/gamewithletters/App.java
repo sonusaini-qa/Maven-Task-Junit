7230b05bb1606936488601299339e7111b18df8a
AbhishekBansal_qait <abhishekbansal@qainfotech.com>
2019-02-11 10:03:15 +0530

    maven task junit

---
+++ maven-task-junit-master/maven-project/src/main/java/gamewithletters/App.java
@@ -0,0 +1,37 @@
+package gamewithletters;
+
+import java.util.ArrayList;
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+ // This is the task in which we have to test our code using Junit
+public class App 
+{
+    public String consecutive (String s)
+    {
+
+        System.out.println("hahahaahah");
+        if(s.equalsIgnoreCase("Abhishek"))
+        {
+            return "Matched";
+        }
+
+    return "Not-found";
+    }
+
+
+
+        
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+        App obj = new App();
+        //String actuall =obj.consecutive("harshit");
+        //System.out.println(actuall);
+    }
+}
